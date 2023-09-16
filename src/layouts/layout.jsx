44c0ab3205645c5e901e45bee6f8ca4310b87312
djsnikeex <nicolasonores@hotmail.com>
2023-09-16 19:17:14 -0300

    Update layout.jsx

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/Footer'
+import Header from '../components/header.jsx'
+import Footer from '../components/Footer.jsx'
 import {Outlet} from 'react-router-dom'
 
 const layout = () => {
